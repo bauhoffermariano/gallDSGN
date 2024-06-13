33a0fcbf02e345cfb2dc2922e60b4a7ac7365fd4
bauhoffermariano <136924609+bauhoffermariano@users.noreply.github.com>
2024-06-13 00:47:10 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const navLogo= document.getElementById('nav__btn');
-const toggleClass= function (){
-    navLogo.classList.toggle('desplegable');
-}
-
-navLogo.addEventListener('click', toggleClass())
-
-console.log('hola')
